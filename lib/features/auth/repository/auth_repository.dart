584a9882fd80db4cd2c0fb8e7acf58ee46d0a12a
axis1410 <1410axis@gmail.com>
2023-10-16 12:02:10 +0530

    Implemented Google Sign in and Initial Routing

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -3,10 +3,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+import "package:reddit_clone/core/providers/firebase_providers.dart";
 import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/core/constants/firebase_constants.dart';
-import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/core/failure.dart';
+import 'package:reddit_clone/core/type_defs.dart';
 import 'package:reddit_clone/models/user_model.dart';
 
 final authRepositoryProvider = Provider(
@@ -31,8 +35,9 @@ class AuthRepository {
         _googleSignIn = googleSignIn;
 
   CollectionReference get _users => _firestore.collection(FirebaseConstants.usersCollection);
+  Stream<User?> get authStateChange => _auth.authStateChanges();
 
-  void signInWithGoogle() async {
+  FutureEither<UserModel> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
 
@@ -44,7 +49,7 @@ class AuthRepository {
       );
 
       UserCredential userCredential = await _auth.signInWithCredential(credential);
-      UserModel userModel;
+      late UserModel userModel;
 
       if (userCredential.additionalUserInfo!.isNewUser) {
         userModel = UserModel(
@@ -57,9 +62,20 @@ class AuthRepository {
           awards: [],
         );
         await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      } else {
+        userModel = await getUserData(userCredential.user!.uid).first;
       }
+      return right(userModel);
+    } on FirebaseException catch (err) {
+      throw err.message!;
     } catch (err) {
-      print(err);
+      return left(Failure(err.toString()));
     }
+  }
+
+  Stream<UserModel> getUserData(String uid) {
+    return _users.doc(uid).snapshots().map(
+          (event) => UserModel.fromMap(event.data() as Map<String, dynamic>),
+        );
   }
 }
