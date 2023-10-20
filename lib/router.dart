ea7f6de569dd5e596976cc6b693ddecea89b82d6
axis1410 <1410axis@gmail.com>
2023-10-20 11:20:14 +0530

    Added "add mods" feature

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/community/screens/add_mods_screen.dart';
 import 'package:reddit_clone/features/community/screens/community_screen.dart';
 import 'package:reddit_clone/features/community/screens/edit_community_screen.dart';
 import 'package:reddit_clone/features/community/screens/mod_tools_screen.dart';
@@ -25,5 +26,8 @@ final loggedInRoute = RouteMap(
     '/edit-community/:name': (routeData) => MaterialPage(
           child: EditCommunityScreen(name: routeData.pathParameters['name']!),
         ),
+    '/add-mods/:name': (routeData) => MaterialPage(
+          child: AddModsScreen(name: routeData.pathParameters['name']!),
+        )
   },
 );
